442032b8f35ef2a82bda3a5d1d8fd3c6f4552cfa
JohnYakuzer <country620@protonmail.com>
2024-06-14 02:47:47 +0200

    Add files via upload

---
+++ Linux Build/connect4-start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# If the programm is not starting, perhaps your python interpreter is on different location or missing
+# change this location_path
+PYTHON_Interpreter=/usr/bin/python3
+
+PYTHON_SCRIPT=./start-game.py
+
+
+$PYTHON $PYTHON_SCRIPT
+
